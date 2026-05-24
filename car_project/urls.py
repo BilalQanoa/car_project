31582dcf4f8039b6661ba582aa13bfffcfb016ee
BilalQanoa <bilalqanoa@gmail.com>
2026-05-25 02:00:23 +0300

    build apps and connect them with project urls

--- car_project/urls.py
+++ car_project/urls.py
@@ -1,8 +1,10 @@
 
-from django.contrib import admin
+from django.contrib import admin, include
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path('', include('main.url')),
+    path('accounts/', include('accounts.url')),
+    path('showcars/', include('showcars.url')),
 ]
